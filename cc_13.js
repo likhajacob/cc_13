eeef7086cff937a1b03d7335c2caf10c84519cf7
Likha Jacob <likha.jacob03@gmail.com>
2025-03-09 17:45:01 -0400

    Task 2 - Employee cards Dynamic Addition

--- cc_13.js
+++ cc_13.js
@@ -1,6 +1,5 @@
 // Task 2 - Adding employee cards dynamically
 function addEmployee(name, position) {
-
     // Create a new element
      // container div
     const container = document.getElementById('employeeContainer');
@@ -8,33 +7,29 @@ function addEmployee(name, position) {
     const employee = document.createElement('div'); 
     // Add a class to the div
     employee.classList.add('employee-card'); 
-
     // Create a new h4 element
     const cardName = document.createElement('h4'); 
     cardName.textContent = name;
-
     // Create a new p element
-    const cardPosition = document.createElement('p'); // Create a new p element
-    cardPosition.textContent = position; // Set the position of the p elementq
-
-    // Create a new button element
+    const cardPosition = document.createElement('p');
+    //Set the position of p elementq
+    cardPosition.textContent = position;
+    //Create a new button element
     const removeButton = document.createElement('button');
     removeButton.textContent = 'Remove Employee'; 
     removeButton.classList.add('remove-Button'); 
-
-    // Add event listener to remove button
+    //Add event listener to remove button
     removeButton.addEventListener('click', () => { 
         container.removeChild(employee); 
     });
-
-    // Append the elements to the container
+    //Append the elements to the container
     employee.appendChild(cardName);
     employee.appendChild(cardPosition);
     employee.appendChild(removeButton); 
-// Append div to container
+//Append div to container
     container.appendChild(employee);
 }
-// Call the function to add employee cards
+//Call the function to add employee cards
 addEmployee('Krupa Jacob', 'Business Analyst at Sephora'); 
 addEmployee('Likha Jacob', 'Advisor at HCC');
 addEmployee('Betty Figi', 'Nurse at TGH');
